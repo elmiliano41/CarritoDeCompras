7e591894a4744c178c6f89ad75c0cce840057b1e
esanchez411 <esanchez@arkusnexus.com>
2024-01-09 15:21:23 -0600

    Cart

---
+++ products.js
@@ -0,0 +1,122 @@
+const products = [
+    {
+      "id": 30,
+      "name": "Key Holder",
+      "price": 30,
+      "image": "https://i.dummyjson.com/data/products/30/thumbnail.jpg",
+      "description": "Attractive DesignMetallic materialFour key hooksReliable & DurablePremium Quality"
+    },
+    {
+      "id": 6,
+      "name": "MacBook Pro",
+      "price": 1749,
+      "image": "https://i.dummyjson.com/data/products/6/thumbnail.png",
+      "description": "MacBook Pro 2021 with mini-LED display may launch between September, November"
+    },
+    {
+      "id": 2,
+      "name": "iPhone X",
+      "price": 899,
+      "image": "https://i.dummyjson.com/data/products/2/thumbnail.jpg",
+      "description": "SIM-Free, Model A19211 6.5-inch Super Retina HD display with OLED technology A12 Bionic chip with ..."
+    },
+    {
+      "id": 3,
+      "name": "Samsung Universe 9",
+      "price": 1249,
+      "image": "https://i.dummyjson.com/data/products/3/thumbnail.jpg",
+      "description": "Samsung's new variant which goes beyond Galaxy to the Universe"
+    },
+    {
+        "id": 4,
+        "name": "OPPOF19",
+        "price": 280,
+        "image": "https://i.dummyjson.com/data/products/4/thumbnail.jpg",
+        "description": "OPPO F19 is officially announced on April 2021."
+      },
+      {
+        "id": 5,
+        "name": "Huawei P30",
+        "price": 499,
+        "image": "https://i.dummyjson.com/data/products/5/thumbnail.jpg",
+        "description": "Huawei’s re-badged P30 Pro New Edition was officially unveiled yesterday in Germany and now the device has made its way to the UK."
+      },
+      {
+        "id": 7,
+        "name": "Samsung Galaxy Book",
+        "price": 1499,
+        "image": "https://i.dummyjson.com/data/products/7/thumbnail.jpg",
+        "description": "Samsung Galaxy Book S (2020) Laptop With Intel Lakefield Chip, 8GB of RAM Launched"
+      },
+      {
+        "id": 1,
+        "name": "iPhone 9",
+        "price": 549,
+        "image": "https://i.dummyjson.com/data/products/1/thumbnail.jpg",
+        "description": "An apple mobile which is nothing like apple"
+      },
+      {
+        "id": 8,
+        "name": "Microsoft Surface Laptop 4",
+        "price": 1499,
+        "image": "https://i.dummyjson.com/data/products/8/thumbnail.jpg",
+        "description": "Style and speed. Stand out on HD video calls backed by Studio Mics. Capture ideas on the vibrant touchscreen."
+      },
+      {
+        "id": 9,
+        "name": "Infinix INBOOK",
+        "price": 1099,
+        "image": "https://i.dummyjson.com/data/products/9/thumbnail.jpg",
+        "description": "Infinix Inbook X1 Ci3 10th 8GB 256GB 14 Win10 Grey – 1 Year Warranty"
+      },
+      {
+        "id": 10,
+        "name": "HP Pavilion 15-DK1056WM",
+        "price": 1099,
+        "image": "https://i.dummyjson.com/data/products/10/thumbnail.jpeg",
+        "description": "HP Pavilion 15-DK1056WM Gaming Laptop 10th Gen Core i5, 8GB, 256GB SSD, GTX 1650 4GB, Windows 10"
+      },
+      {
+        "id": 11,
+        "name": "perfume Oil",
+        "price": 13,
+        "image": "https://i.dummyjson.com/data/products/11/thumbnail.jpg",
+        "description": "Mega Discount, Impression of Acqua Di Gio by GiorgioArmani concentrated attar perfume Oil"
+      },
+      {
+        "id": 12,
+        "name": "Brown Perfume",
+        "price": 40,
+        "image": "https://i.dummyjson.com/data/products/12/thumbnail.jpg",
+        "description": "Royal_Mirage Sport Brown Perfume for Men & Women - 120ml"
+      },
+      {
+        "id": 13,
+        "name": "Fog Scent Xpressio Perfume",
+        "price": 13,
+        "image": "https://i.dummyjson.com/data/products/13/thumbnail.webp",
+        "description": "Product details of Best Fog Scent Xpressio Perfume 100ml For Men cool long lasting perfumes for Men"
+      },
+      {
+        "id": 14,
+        "name": "Non-Alcoholic Concentrated Perfume Oil",
+        "price": 120,
+        "image": "https://i.dummyjson.com/data/products/14/thumbnail.jpg",
+        "description": "Original Al Munakh® by Mahal Al Musk | Our Impression of Climate | 6ml Non-Alcoholic Concentrated Perfume Oil"
+      },
+      {
+        "id": 15,
+        "name": "Eau De Perfume Spray",
+        "price": 30,
+        "image": "https://i.dummyjson.com/data/products/15/thumbnail.jpg",
+        "description": "Genuine Al-Rehab spray perfume from UAE/Saudi Arabia/Yemen High Quality"
+      },
+      {
+        "id": 16,
+        "name": "Hyaluronic Acid Serum",
+        "price": 19,
+        "image": "https://i.dummyjson.com/data/products/16/thumbnail.jpg",
+        "description": "L'OrÃ©al Paris introduces Hyaluron Expert Replumping Serum formulated with 1.5% Hyaluronic Acid"
+      },
+]
+export default products;
